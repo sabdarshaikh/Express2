de9fb371f49b5284f80421ac76cf2290cce68256
Sabdar <sabdarshaikh10@gmai.com>
2024-07-11 21:23:06 +0530

    completed setup of EJS template and passing data to Views

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+// Config ejs 
+app.set('view engine', 'ejs');
+app.set('views', 'views')
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Welcome',
+        message: 'HEllo from EJS',
+        people: ['John', 'Jane', 'Jack'],
+    });
+});
+
+app.listen(8000, () => console.log('Server Started'));
+ 
